3d1aa73c786a4f52c02dd89ce45029ede2c377e8
Martin Lowe <59805940+Martin2251@users.noreply.github.com>
2023-08-09 18:14:11 +0100

    new tickets

---
+++ app/page.jsx
@@ -0,0 +1,9 @@
+
+
+export default function Home() {
+  return (
+    <main>
+      <h2>dashboard</h2>
+    </main>
+  )
+}
---
+++ app/tickets/page.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Tickets = () => {
+  return (
+    <main>Tickets</main>
+  )
+}
+
+export default Tickets
