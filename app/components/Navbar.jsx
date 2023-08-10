1cabc077e0c0aa3daa0cc2e658da3c8d05037b9b
Martin Lowe <59805940+Martin2251@users.noreply.github.com>
2023-08-10 07:47:15 +0100

    added image

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -1,8 +1,12 @@
-import React from 'react'
+import Image from 'next/image'
 import Link from 'next/link'
+import Logo from './dojo-logo.png'
+
 function Navbar() {
   return (
-    <nav><h1>Helpdesk</h1>
+    <nav>
+    <Image src={Logo} alt="logo" width={70} placeholder='blur' />
+    <h1>Helpdesk</h1>
     <Link href="/">Home</Link>
     <Link href="/tickets">Tickets</Link>
     </nav>
