e2ec98ddfd51027681eb96b5b38b4f5f13a60dcb
Martin Lowe <59805940+Martin2251@users.noreply.github.com>
2023-08-10 08:01:24 +0100

    ticketlist

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -1,16 +1,20 @@
-import Image from 'next/image'
 import Link from 'next/link'
+import Image from 'next/image'
 import Logo from './dojo-logo.png'
 
-function Navbar() {
+export default function Navbar() {
   return (
     <nav>
-    <Image src={Logo} alt="logo" width={70} placeholder='blur' />
-    <h1>Helpdesk</h1>
-    <Link href="/">Home</Link>
-    <Link href="/tickets">Tickets</Link>
+      <Image
+        src={Logo}
+        alt='Dojo Helpdesk logo'
+        width={70}
+        placeholder='blur'
+        quality={100}
+      />
+      <h1>Dojo Helpdesk</h1>
+      <Link href="/">Dashboard</Link>
+      <Link href="/tickets">Tickets</Link>
     </nav>
   )
 }
-
-export default Navbar
