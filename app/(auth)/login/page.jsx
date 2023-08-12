cf07d8da0b760ad7662c4b2e006fb4f9c5290030
Martin Lowe <59805940+Martin2251@users.noreply.github.com>
2023-08-12 10:08:51 +0100

    route groups

---
+++ app/(auth)/login/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Login() {
+  return (
+<main>
+    <h2 className="text-center">Log In</h2>
+
+
+</main>
+  )
+}
---
+++ app/(auth)/signup/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Signup() {
+  return (
+<main>
+    <h2 className="text-center">Sign Up</h2>
+
+
+</main>
+  )
+}
