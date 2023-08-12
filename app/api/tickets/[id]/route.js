8372894bdb3845a3f63943ab2bb494a6d691484a
Martin Lowe <59805940+Martin2251@users.noreply.github.com>
2023-08-12 11:47:49 +0100

    route handlers

---
+++ app/api/tickets/[id]/route.js
@@ -0,0 +1,26 @@
+// fetching data from client component
+
+
+import { NextResponse } from "next/server"
+
+export const dynamic = 'force-dynamic'
+
+
+export async function GET(_, {params}){
+    const id = params.id
+    const res = await fetch (`http://localhost:4000/tickets/${id}`)
+
+    // get ticket
+    const ticket = await res.json()
+
+    if(!res.ok){
+        return NextResponse.json({error:"Cannot find the ticket"},{
+            status:404
+        }
+        )
+    }
+
+    return NextResponse.json(ticket,{
+        status:200,
+    })
+}
