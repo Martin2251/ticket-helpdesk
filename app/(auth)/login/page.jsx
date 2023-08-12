02a25aa3bcf9a824bedd822adff7c18b158d7611
Martin Lowe <59805940+Martin2251@users.noreply.github.com>
2023-08-12 19:48:53 +0100

    auth form

--- app/(auth)/login/page.jsx
+++ app/(auth)/login/page.jsx
@@ -1,10 +1,17 @@
 import React from 'react'
+import AuthForm from '../AuthForm'
+import { Passero_One } from 'next/font/google'
 
+const handleSubmit = async (e,email,password) => {
+  e.preventDefaul ()
+
+  console.log(email,password)
+}
 export default function Login() {
   return (
 <main>
     <h2 className="text-center">Log In</h2>
-
+<AuthForm  />
 
 </main>
   )
