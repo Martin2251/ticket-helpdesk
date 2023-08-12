b307efc3a4cec87bc3777108bd459705ec83d9d8
Martin Lowe <59805940+Martin2251@users.noreply.github.com>
2023-08-12 09:37:47 +0100

    new 404

---
+++ app/not-found.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Link from 'next/link'
+
+function NotFound() {
+  return (
+    <main className='text-center'>
+        <h2 className='text-3xl'>There was a problem.</h2>
+        <p> we could not find the page you were looking for </p>
+        <p> go back to the <Link href="/">Dashboard</Link></p>
+    </main>
+  )
+}
+
+export default NotFound
---
+++ app/tickets/not-found.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Link from 'next/link'
+
+function NotFound() {
+  return (
+    <main className='text-center'>
+        <h2 className='text-3xl'>There was a problem.</h2>
+        <p> we could not find the ticket you were looking for </p>
+        <p> go back to all the  <Link href="/tickets">Tickets</Link></p>
+    </main>
+  )
+}
+
+export default NotFound
