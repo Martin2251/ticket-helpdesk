84375b07353ee30f5089e07480aed67eb8b6f28f
Martin Lowe <59805940+Martin2251@users.noreply.github.com>
2023-08-15 07:47:34 +0100

    fetching single records

--- app/(dashboard)/tickets/[id]/page.jsx
+++ app/(dashboard)/tickets/[id]/page.jsx
@@ -1,61 +1,53 @@
-import { notFound } from "next/navigation"
-import React from 'react'
-
+import { notFound } from 'next/navigation'
+import { cookies } from 'next/headers'
+import { createServerComponentClient } from '@supabase/auth-helpers-nextjs'
 
 export const dynamicParams = true
 
-export async function generateMetadata({params}){
-  const id = params.id
+export async function generateMetadata({ params }) {
+  const supabase = createServerComponentClient({ cookies })
 
-  const res= await fetch(`http://localhost:4000/tickets/${id}`)
-  const ticket = await res.json()
+  const { data: ticket } = await supabase.from('tickets')
+    .select()
+    .eq('id', params.id)
+    .single()
+ 
   return {
-    title: `Helpdesk |${ticket.title}`
+    title: `Dojo Helpdesk | ${ticket?.title || 'Ticket not Found'}`
   }
 }
 
-export async function generateStaticParams () {
-    const res = await fetch('http://localhost:4000/tickets')
-    const tickets = await res.json()
+async function getTicket(id) {
+  const supabase = createServerComponentClient({ cookies })
 
-    return tickets.map((ticket) => ({
-        id:ticket.id
-    }))
-}
+  const { data } = await supabase.from('tickets')
+    .select()
+    .eq('id', id)
+    .single()
 
-
-async function getTickets(id) {
-    // imitate delay
-    await new Promise(resolve => setTimeout(resolve,3000))
-    const res = await fetch('http://localhost:4000/tickets/' + id, {
-      next: {
-        revalidate: 60 // use 0 to opt out of using cache
-      }
-    })
-
-    if(!res.ok){
-        notFound()
+    if (!data) {
+      notFound()
     }
   
-    return res.json()
-  }
-export default  async function TicketDetails({params}) {
-    const ticket = await getTickets(params.id)
-   
+    return data
+}
+
+export default async function TicketDetails({ params }) {
+  const ticket = await getTicket(params.id)
+
   return (
- <main>
-    <nav>
+    <main>
+      <nav>
         <h2>Ticket Details</h2>
-        <div className='card'>
-            <h3>{ticket.title}</h3>
-            <small>Created by {ticket.user_email}</small>
-            <p>{ticket.body}</p>
-            <div className={`pill ${ticket.priority}`}>
-              {ticket.priority} priority
-            </div>
+      </nav>
+      <div className="card">
+        <h3>{ticket.title}</h3>
+        <small>Created by {ticket.user_email}</small>
+        <p>{ticket.body}</p>
+        <div className={`pill ${ticket.priority}`}>
+          {ticket.priority} priority
         </div>
-    </nav>
- </main>
-    
+      </div>
+    </main>
   )
 }
